99224eef3332bb04b99cd2095eff7a4089d08b73
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-03-05 11:32:43 +0100

    name change

--- day4.py
+++ day4.py
@@ -5,11 +5,11 @@ import pandas as pd
 import unittest
 
 
-def gerund_infinitive1(word):
+def gerund_infinitive_v1(word):
     return 'to ' + word[:-3] if word[-3::] == 'ing' else 'To nie jest rzeczownik odsłowny'
 
 
-def gerund_infinitive2(word, listofverbs):
+def gerund_infinitive_v2(word, listofverbs):
     if not(len(word) < 4 or word[-3::] != 'ing'):        
         verb = word[:-3]
         if verb in listofverbs:
@@ -23,17 +23,17 @@ def gerund_infinitive2(word, listofverbs):
 
 class TestAppMethods(unittest.TestCase):
     def test1(self):
-        self.assertEqual(gerund_infinitive1('doing'), 'to do')
-        self.assertEqual(gerund_infinitive1('having'), 'to hav')
-        self.assertEqual(gerund_infinitive1('habit'), 'To nie jest rzeczownik odsłowny')
+        self.assertEqual(gerund_infinitive_v1('doing'), 'to do')
+        self.assertEqual(gerund_infinitive_v1('having'), 'to hav')
+        self.assertEqual(gerund_infinitive_v1('habit'), 'To nie jest rzeczownik odsłowny')
     
     def test2(self):
         verblist = pd.read_csv('day4data.csv', sep=',')
         verblist = list(verblist['verb'])
-        self.assertEqual(gerund_infinitive2('doing', verblist), 'to do')
-        self.assertEqual(gerund_infinitive2('having', verblist), 'to have')
-        self.assertEqual(gerund_infinitive2('omitting', verblist), 'to omit')
-        self.assertEqual(gerund_infinitive2('habit', verblist), 'To nie jest rzeczownik odsłowny')
+        self.assertEqual(gerund_infinitive_v2('doing', verblist), 'to do')
+        self.assertEqual(gerund_infinitive_v2('having', verblist), 'to have')
+        self.assertEqual(gerund_infinitive_v2('omitting', verblist), 'to omit')
+        self.assertEqual(gerund_infinitive_v2('habit', verblist), 'To nie jest rzeczownik odsłowny')
 
 
 if __name__ == '__main__':
