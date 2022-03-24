c1e80f9dbec5aebb7191e13143d6cbd628205ffa
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-03-24 22:08:01 +0100

    done day 24

---
+++ day24.py
@@ -0,0 +1,30 @@
+''' Solution to task from https://www.instagram.com/p/CbcA5qfAurB/ '''
+
+
+from typing import List
+import unittest
+
+
+def is_geometric(nums: List[int]) -> bool:
+    if len(nums) > 1:
+        q = nums[1] / nums[0]
+        for index in range(1, len(nums) - 1):
+            if nums[index + 1] / nums[index] != q:
+                return False
+        return True
+    return False
+
+
+
+class TestAppMethods(unittest.TestCase):
+    def testing_is_geometric(self):
+        self.assertEqual(is_geometric([1, 2, 4, 8]), True)
+        self.assertEqual(is_geometric([1, 2, 4, 9]), False)
+        self.assertEqual(is_geometric([1]), False)
+        self.assertEqual(is_geometric([1, 3, 4, 9]), False)
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+    
