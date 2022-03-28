4ace2466f8bc7db012699bceb83392f95e19e7c5
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-03-28 20:19:51 +0200

    added second solution to day 28

--- day28.py
+++ day28.py
@@ -3,19 +3,33 @@
 
 import unittest
 
-
-def digit_adder(num: int) -> int:
-    return -1 if num < 0 else \
+# one-liner
+def digit_adder_v1(num: int) -> int:
+    return (-1 if num < 0 else 1 ) * \
         int(''.join([str(int(x) + 1) for x in list(str(num))]))
     
+
+def digit_adder_v2(num: int) -> int:
+    if num < 0:
+        retval = '-'
+        num *= -1
+    else:
+        retval = ''
+    for digit in str(num):
+        retval += str(int(digit) + 1)
+    return int(retval)
 
 
 
 class TestAppMethods(unittest.TestCase):
-    def testing_digit_adder(self):
-        self.assertEqual(digit_adder(998), 10109)
-        self.assertEqual(digit_adder(10109), 212110)
+    def testing_digit_adder_v1(self):
+        self.assertEqual(digit_adder_v1(998), 10109)
+        self.assertEqual(digit_adder_v1(10109), 212110)
         
+    
+    def testing_digit_adder_v2(self):
+        self.assertEqual(digit_adder_v2(998), 10109)
+        self.assertEqual(digit_adder_v2(10109), 212110)
         
         
 if __name__ == '__main__':
