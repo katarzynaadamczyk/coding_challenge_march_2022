bff8d8a80a32b80ad941ad4a330d51da5c7bedba
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-04-01 21:22:43 +0200

    done second solution, tests and time counting

--- day31.py
+++ day31.py
@@ -1,28 +1,39 @@
 ''' Solution to task from https://www.instagram.com/p/CbwenENg-G9/ '''
 
-from email.policy import default
+import time
 from typing import Optional
 import unittest
 
-# one-liner
+def measuretime(func):
+    def wrapper(*args, **kwargs):
+        starttime = time.perf_counter_ns()
+        ret = func(*args, **kwargs)
+        endtime = time.perf_counter_ns()
+        print(f'Time needed: {endtime - starttime} ns')
+        return ret
+    return wrapper
+
+
+# one-liner, o(n)
+@measuretime
 def count_ones_v1(num: int) -> int:
     return sum([str(x).count('1') for x in range(num + 1)])
 
-# second solution
+# second solution, o(log(n)) 
+@measuretime
 def count_ones_v2(num: int) -> int:
-    ones, power, default, prev_rest = 0, 0, 0, 0
+    original_num, power, ones, lst_defaults = num, 0, 0, [0]
     while num > 0:
-        print(num, ones, power, default, prev_rest)
-        if num % 10 > 1:
-            ones += 10 ** power
-        elif num % 10 == 1:
-            ones += 1 + prev_rest * 10 ** (power - 1)
-        default += 10 ** power
-        power += 1
-        prev_rest = num % 10
+        lst_defaults.append(10 ** power + 10 * lst_defaults[-1])
         num //= 10
-    
-    print(num, ones, power, default, prev_rest)
+        power += 1
+    for index in range(len(lst_defaults) - 1, -1, -1):
+        ones += original_num // 10 ** index * lst_defaults[index]
+        if original_num // 10 ** index == 1:
+            ones += 1 + original_num % 10 ** index
+        elif original_num // 10 ** index > 1:
+            ones += 10 ** index
+        original_num -= original_num // 10 ** index * 10 ** index
     return ones
     
 
@@ -35,12 +46,20 @@ class TestAppMethods(unittest.TestCase):
         self.assertEqual(count_ones_v1(100), 21)
         
     def testing_count_ones_v2(self):
+        self.assertEqual(count_ones_v2(100), 21)
+        self.assertEqual(count_ones_v2(19), 12)
         self.assertEqual(count_ones_v2(1), 1)
         self.assertEqual(count_ones_v2(10), 2)
-        self.assertEqual(count_ones_v2(19), 12)
-        self.assertEqual(count_ones_v2(100), 21)
-        
+        self.assertEqual(count_ones_v2(1020584), count_ones_v1(1020584))
+
+
+def main():
+    print('v1:', count_ones_v1(1020584))
+    print('v2:', count_ones_v2(1020584))
         
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    main()
+    
+    
     
